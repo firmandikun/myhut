5c8d88e07030075dedcc2a24756371227dfa5cf4
firman <firman@technopartner.co.id>
2025-05-18 00:25:41 +0700

    feat: add logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,9 +4,7 @@
     </button>
     <div>
         <a href="{{ url('/') }}" class="sidebar-logo">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="site logo" class="light-logo">
-            <img src="{{ asset('assets/images/logo-light.png') }}" alt="site logo" class="dark-logo">
-            <img src="{{ asset('assets/images/logo-icon.png') }}" alt="site logo" class="logo-icon">
+            <h2>My Hut</h2>
         </a>
     </div>
     <div class="sidebar-menu-area">
